38e37685c5c42cdaceec1f4c713301b1a15fbcc5
Saish Pudasaini <pudasainisaish24@gmail.com>
2026-01-22 10:24:21 +0000

    compiletime ---> run-time

--- WorkshopWeek12/StudentApp.java
+++ WorkshopWeek12/StudentApp.java
@@ -1,36 +1,57 @@
 package WorkshopWeek12;
 import java.util.*;
 
-public class StudentApp{
+public class StudentApp {
 
     static ArrayList<Student> students = new ArrayList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         addData();
         removeData();
-        getSetData();
+        updateData();
         displayData();
     }
 
-    public static void addData(){
+    public static void addData() {
         students.add(new Student("Ayush", 9876543210L, "L1N1"));
         students.add(new Student("Rahul", 9876543211L, "L1M1"));
         students.add(new Student("Gaurav", 9876543212L, "L1N2"));
         students.add(0, new Student("Aavaya", 9876543213L, "L1C1"));
     }
 
-    public static void removeData(){
-        students.remove(2);
-    }
+    public static void removeData() {
+        String removeName = "Gaurav";
 
-    public static void getSetData(){
-        students.get(2).setGroup("L1N2");      
-        students.get(1).setName("Aayush"); 
-    }
-
-    public static void displayData(){
-        for(Student z : students){
-            z.displayStudentDetails();
+        Iterator<Student> it = students.iterator();
+        while (it.hasNext()) {
+            Student s = it.next();
+            if (s.getName().equals(removeName)) {
+                it.remove();
+                break;
+            }
         }
-    }    
+    }
+
+    public static void updateData() {
+
+        for (Student s : students) {
+            if (s.getName().equals("Rahul")) {
+                s.setGroup("L1N2");
+                break;
+            }
+        }
+
+        for (Student s : students) {
+            if (s.getName().equals("Ayush")) {
+                s.setName("Aayush");
+                break;
+            }
+        }
+    }
+
+    public static void displayData() {
+        for (Student s : students) {
+            s.displayStudentDetails();
+        }
+    }
 }
